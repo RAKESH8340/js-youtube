2f63e0352474d959f46a1f46f5b1a6ecf1dee30b
RAKESH8340 <146659870+RAKESH8340@users.noreply.github.com>
2025-01-21 18:24:13 +0000

    container config done

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,6 +2,8 @@ const accountId = 158734
 let accountEmail = "rk8473791@gmail.com"
 var accountPassword ="45673"
 
+let accountState;
+
 // accountId = 2 not allowd 
 
 accountEmail = "rk8745@gmail.com"
@@ -13,4 +15,4 @@ accountCity = "Ranchi"
 // console.log(accountPassword);
 // console.log(accountCity);
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

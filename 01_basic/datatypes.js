00841402eba1c696a2d9457afdaf75e78d255ec0
RAKESH8340 <146659870+RAKESH8340@users.noreply.github.com>
2025-01-26 17:52:41 +0000

    container config done

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,41 @@
+"use strict" // treat all js code as newer version
+
+// alert (3+3) we are using node js, not browser
+
+console.log(3
+    +
+    3) // code redeability should be high
+
+    console.log("Rakesh")
+
+    let name = "Raj"
+    let age = 18
+    let IsLoggedIn = false
+
+
+    //   NUMBER => 2 TO POWER 53
+    //   BIGINT
+    //   STRING =""
+    //   Boolean = True / false
+    //   NULL =>  STANDLONE VALUE
+    //   UNDEFINED => 
+    //   SYMBOL => UNIQUE VALUE
+    //   OBJECT
+
+    console.log(typeof null);
+
+//   let x = 2 // number
+    //   let y = "2" // string
+    //   let z = true // boolean
+    //   console.log(x + y) // concatenation
+    //   console.log(x == y) // true, because string to number conversion is done
+    //   console.log(x === y) // false, because data type is different
+
+    //   let a = 10
+    //   let b = 5
+    //   console.log(a - b) // 5
+    //   console.log(a * b) // 50
+    //   console.log(a / b) // 2
+    //   console.log(a % b) // 0
+
+    //   let myNumber = 10.5

c2db75fd59cd1410032ff1e671723d6f2ab0838a
RAKESH8340 <146659870+RAKESH8340@users.noreply.github.com>
2025-01-26 18:13:45 +0000

    container config done

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "50abc"
+
+//console.log (typeof score);
+//console.log (typeof(score));
+
+let valueInNumber = Number(score)
+//console.log (typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "50" => 33
+// "50abc" => NaN
+// "true" => 1; false => 0
+
+let isLoggedIn = 0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" =>  false
+// "rakesh" => true
+
+let someNumber = 50
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
